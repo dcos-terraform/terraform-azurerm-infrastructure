5623c566d0069274a86ebf1c3720638bdc8746e9
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2018-08-10 17:26:55 -0700

    init

--- main.tf
+++ main.tf
@@ -16,15 +16,17 @@ module "bootstrap" {
 
   # version = "0.0.0"
 
-  disk_size                 = "${var.gcp_bootstrap_disk_size}"
-  disk_type                 = "${var.gcp_bootstrap_disk_type}"
-  region                    = "${var.gcp_region}"
-  machine_type              = "${var.gcp_bootstrap_gce_type}"
+  disk_size                 = "${coalesce(var.gcp_bootstrap_disk_size, var.infra_disk_size)}"
+  disk_type                 = "${coalesce(var.gcp_bootstrap_disk_type, var.infra_disk_type)}"
+  machine_type              = "${coalesce(var.gcp_bootstrap_gce_type, var.infra_machine_type)}"
   cluster_name              = "${var.cluster_name}"
-  public_ssh_key            = "${var.gcp_bootstrap_public_ssh_key}"
-  ssh_user                  = "${var.gcp_bootstrap_ssh_user}"
+  public_ssh_key            = "${coalesce(var.gcp_bootstrap_public_ssh_key, var.infra_public_ssh_key)}"
+  ssh_user                  = "${coalesce(var.gcp_bootstrap_ssh_user, var.infra_ssh_user)}"
   bootstrap_subnetwork_name = "${module.network.agent_subnetwork_name}"
   image                     = "${var.gcp_bootstrap_image}"
+  dcos_instance_os = "${coalesce(var.gcp_bootstrap_dcos_instance_os, var.infra_dcos_instance_os)}"
+  # Determine if we need to force a particular region
+  zone_list = "${data.google_compute_zones.available.names}"
 }
 
 module "masters" {
@@ -33,15 +35,15 @@ module "masters" {
   # version = "0.0.0"
 
   num_masters            = "${var.num_masters}"
-  disk_size              = "${var.gcp_master_disk_size}"
-  disk_type              = "${var.gcp_master_disk_type}"
-  region                 = "${var.gcp_region}"
-  machine_type           = "${var.gcp_master_gce_type}"
+  disk_size              = "${coalesce(var.gcp_master_disk_size, var.infra_disk_size)}"
+  disk_type              = "${coalesce(var.gcp_master_disk_type, var.infra_disk_type)}"
+  machine_type           = "${coalesce(var.gcp_master_gce_type, var.infra_machine_type)}"
   cluster_name           = "${var.cluster_name}"
-  public_ssh_key         = "${var.gcp_master_public_ssh_key}"
-  ssh_user               = "${var.gcp_master_ssh_user}"
+  public_ssh_key         = "${coalesce(var.gcp_master_public_ssh_key, var.infra_public_ssh_key)}"
+  ssh_user               = "${coalesce(var.gcp_master_ssh_user, var.infra_ssh_user)}"
   master_subnetwork_name = "${module.network.master_subnetwork_name}"
   image                  = "${var.gcp_master_image}"
+  dcos_instance_os = "${coalesce(var.gcp_master_dcos_instance_os, var.infra_dcos_instance_os)}"
   # Determine if we need to force a particular region
   zone_list = "${data.google_compute_zones.available.names}"
 }
@@ -52,15 +54,15 @@ module "private-agent" {
   # version = "0.0.0"
 
   num_private_agents            = "${var.num_private_agents}"
-  disk_size                     = "${var.gcp_private_agent_disk_size}"
-  disk_type                     = "${var.gcp_private_agent_disk_type}"
-  region                        = "${var.gcp_region}"
-  machine_type                  = "${var.gcp_private_agent_gce_type}"
+  disk_size                     = "${coalesce(var.gcp_private_agent_disk_size, var.infra_disk_size)}"
+  disk_type                     = "${coalesce(var.gcp_private_agent_disk_type, var.infra_disk_type)}"
+  machine_type                  = "${coalesce(var.gcp_private_agent_gce_type, var.infra_machine_type)}"
   cluster_name                  = "${var.cluster_name}"
-  public_ssh_key                = "${var.gcp_private_agent_public_ssh_key}"
-  ssh_user                      = "${var.gcp_private_agent_ssh_user}"
+  public_ssh_key                = "${coalesce(var.gcp_private_agent_public_ssh_key, var.infra_public_ssh_key)}"
+  ssh_user                      = "${coalesce(var.gcp_private_agent_ssh_user, var.infra_ssh_user)}"
   private_agent_subnetwork_name = "${module.network.agent_subnetwork_name}"
   image                         = "${var.gcp_private_agent_image}"
+  dcos_instance_os = "${coalesce(var.gcp_priave_agent_dcos_instance_os, var.infra_dcos_instance_os)}"
   # Determine if we need to force a particular region
   zone_list = "${data.google_compute_zones.available.names}"
 }
@@ -71,15 +73,15 @@ module "public-agent" {
   # version = "0.0.0"
 
   num_public_agents            = "${var.num_public_agents}"
-  disk_size                    = "${var.gcp_public_agent_disk_size}"
-  disk_type                    = "${var.gcp_public_agent_disk_type}"
-  region                       = "${var.gcp_region}"
-  machine_type                 = "${var.gcp_public_agent_gce_type}"
+  disk_size                    = "${coalesce(var.gcp_public_agent_disk_size, var.infra_disk_size)}"
+  disk_type                    = "${coalesce(var.gcp_public_agent_disk_type, var.infra_disk_type)}"
+  machine_type                 = "${coalesce(var.gcp_public_agent_gce_type, var.infra_machine_type)}"
   cluster_name                 = "${var.cluster_name}"
-  public_ssh_key               = "${var.gcp_ssh_key}"
-  ssh_user                     = "${var.gcp_public_agent_ssh_user}"
+  public_ssh_key               = "${coalesce(var.gcp_public_agent_public_ssh_key, var.infra_public_ssh_key)}"
+  ssh_user                     = "${coalesce(var.gcp_public_agent_ssh_user, var.infra_ssh_user)}"
   public_agent_subnetwork_name = "${module.network.agent_subnetwork_name}"
   image                        = "${var.gcp_public_agent_image}"
+  dcos_instance_os = "${coalesce(var.gcp_public_agent_dcos_instance_os, var.infra_dcos_instance_os)}"
   # Determine if we need to force a particular region
   zone_list = "${data.google_compute_zones.available.names}"
 }
--- variables.tf
+++ variables.tf
@@ -2,79 +2,112 @@
 variable "cluster_name" {}
 
 #
-variable "gcp_bootstrap_disk_size" {}
+variable "gcp_bootstrap_disk_size" { default = "" }
 
 #
-variable "gcp_bootstrap_disk_type" {}
+variable "gcp_bootstrap_disk_type" { default = "" }
 
 #
-variable "gcp_bootstrap_gce_type" {}
+variable "gcp_bootstrap_gce_type" { default = "" }
 
 #
-variable "gcp_bootstrap_image" {}
+variable "gcp_bootstrap_image" { default = "" }
 
 #
-variable "gcp_master_disk_size" {}
+variable "gcp_master_disk_size" { default = "" }
 
 #
-variable "gcp_master_disk_type" {}
+variable "gcp_master_disk_type" { default = "" }
 
 #
-variable "gcp_master_gce_type" {}
+variable "gcp_master_gce_type" { default = "" }
 
 #
-variable "gcp_master_image" {}
+variable "gcp_master_image" { default = "" }
 
 #
-variable "gcp_private_agent_disk_size" {}
+variable "gcp_private_agent_disk_size" { default = "" }
 
 #
-variable "gcp_private_agent_disk_type" {}
+variable "gcp_private_agent_disk_type" { default = "" }
 
 #
-variable "gcp_private_agent_gce_type" {}
+variable "gcp_private_agent_gce_type" { default = "" }
 
 #
-variable "gcp_private_agent_image" {}
+variable "gcp_private_agent_image" { default = "" }
 
 #
-variable "gcp_public_agent_disk_size" {}
+variable "gcp_public_agent_disk_size" { default = "" }
 
 #
-variable "gcp_public_agent_disk_type" {}
+variable "gcp_public_agent_disk_type" { default = "" }
 
 #
-variable "gcp_public_agent_gce_type" {}
+variable "gcp_public_agent_gce_type" { default = "" }
 
 #
-variable "gcp_public_agent_image" {}
+variable "gcp_public_agent_image" { default = "" }
 
 #
-variable "gcp_master_public_ssh_key" {}
+variable "gcp_master_public_ssh_key" { default = "" }
 
 #
-variable "gcp_private_agent_public_ssh_key" {}
+variable "gcp_private_agent_public_ssh_key" { default = "" }
 
 #
-variable "gcp_bootstrap_public_ssh_key" {}
+variable "gcp_bootstrap_public_ssh_key" { default = "" }
 
 #
-variable "gcp_region" {}
+variable "gcp_region" { default = "" }
 
 #
-variable "gcp_ssh_key" {}
+variable "gcp_ssh_key" { default = "" }
 
 #
-variable "gcp_master_ssh_user" {}
+variable "gcp_master_ssh_user" { default = "" }
 
 #
-variable "gcp_bootstrap_ssh_user" {}
+variable "gcp_bootstrap_ssh_user" { default = "" }
 
 #
-variable "gcp_public_agent_ssh_user" {}
+variable "gcp_public_agent_ssh_user" { default = "" }
 
 #
-variable "gcp_private_agent_ssh_user" {}
+variable "gcp_public_agent_public_ssh_key" { default = "" }
+
+#
+variable "gcp_private_agent_ssh_user" { default = "" }
+
+#
+variable "infra_ssh_user" { default = "" }
+
+#
+variable "infra_public_ssh_key" { default = "" }
+
+#
+variable "infra_disk_type" { default = "" }
+
+#
+variable "infra_disk_size" { default = "" }
+
+#
+variable "infra_machine_type" { default = "" }
+
+#
+variable "infra_dcos_instance_os" { default = "" }
+
+#
+variable "gcp_master_dcos_instance_os" { default = "" }
+
+#
+variable "gcp_public_agent_dcos_instance_os" { default = "" }
+
+#
+variable "gcp_priave_agent_dcos_instance_os" { default = "" }
+
+#
+variable "gcp_bootstrap_dcos_instance_os" { default = "" }
 
 ##############################
 #                            #
