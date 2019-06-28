f581dd226a7a2f2869e760bafd4dd6f80753788a
Sebastian Brandt <793580+sebbrandt87@users.noreply.github.com>
2019-06-28 14:47:46 +0200

    added num_bootstrap for now

--- variables.tf
+++ variables.tf
@@ -165,6 +165,11 @@ variable "bootstrap_dcos_instance_os" {
   default     = ""
 }
 
+variable "num_bootstrap" {
+  description = "Specify the amount of bootstrap. You should have at most 1"
+  default     = "1"
+}
+
 variable "num_masters" {
   description = "Specify the amount of masters. For redundancy you should have at least 3"
   default     = "3"
