719036a8a93b7cf8a0b8f436d97ae5cc84dfd637
Jan Ulferts <julferts@mesosphere.com>
2020-06-12 11:21:37 +0200

    feat: upgrade to tf 0.12

--- main.tf
+++ main.tf
@@ -23,24 +23,26 @@
  */
 
 locals {
-  default_subnets = ["${var.subnet_range}"]
+  default_subnets = var.subnet_range
 }
 
 data "null_data_source" "lb_rules" {
-  count = "${length(var.public_agents_additional_ports)}"
+  count = length(var.public_agents_additional_ports)
 
   inputs = {
-    frontend_port = "${element(var.public_agents_additional_ports, count.index)}"
+    frontend_port = element(var.public_agents_additional_ports, count.index)
   }
 }
 
-provider "azurerm" {}
+provider "azurerm" {
+  version = "~> 1.0"
+}
 
 # Create a resource group
 resource "azurerm_resource_group" "rg" {
-  name     = "${var.name_prefix != "" ? "${var.name_prefix}-${var.cluster_name}" : var.cluster_name}"
-  location = "${var.location}"
-  tags     = "${var.tags}"
+  name     = var.name_prefix != "" ? "${var.name_prefix}-${var.cluster_name}" : var.cluster_name
+  location = var.location
+  tags     = var.tags
 }
 
 module "network" {
@@ -48,16 +50,16 @@ module "network" {
   version = "~> 0.2.0"
 
   providers = {
-    azurerm = "azurerm"
+    azurerm = azurerm
   }
 
-  subnet_range = "${var.subnet_range}"
-  cluster_name = "${var.cluster_name}"
-  name_prefix  = "${var.name_prefix}"
-  location     = "${var.location}"
+  subnet_range = var.subnet_range
+  cluster_name = var.cluster_name
+  name_prefix  = var.name_prefix
+  location     = var.location
 
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-  tags                = "${var.tags}"
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = var.tags
 }
 
 module "network-security-group" {
@@ -65,34 +67,35 @@ module "network-security-group" {
   version = "~> 0.2.0"
 
   providers = {
-    azurerm = "azurerm"
+    azurerm = azurerm
   }
 
-  location                       = "${var.location}"
-  subnet_range                   = "${var.subnet_range}"
-  cluster_name                   = "${var.cluster_name}"
-  name_prefix                    = "${var.name_prefix}"
-  admin_ips                      = ["${var.admin_ips}"]
-  public_agents_additional_ports = ["${var.public_agents_additional_ports}"]
-  num_bootstrap                  = "${var.num_bootstrap}"
-  num_masters                    = "${var.num_masters}"
-  num_private_agents             = "${var.num_private_agents}"
-  num_public_agents              = "${var.num_public_agents}"
+  location                       = var.location
+  subnet_range                   = var.subnet_range
+  cluster_name                   = var.cluster_name
+  name_prefix                    = var.name_prefix
+  admin_ips                      = var.admin_ips
+  public_agents_additional_ports = var.public_agents_additional_ports
+  num_bootstrap                  = var.num_bootstrap
+  num_masters                    = var.num_masters
+  num_private_agents             = var.num_private_agents
+  num_public_agents              = var.num_public_agents
 
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-  tags                = "${var.tags}"
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = var.tags
 }
 
 // If External Exhibitor is Specified, Create a Storage Account
 resource "azurerm_storage_account" "external_exhibitor" {
-  count                    = "${var.azurerm_storage_account_name != "" ? 1 : 0}"
-  name                     = "${var.azurerm_storage_account_name}"
-  name_prefix              = "${var.name_prefix}"
-  resource_group_name      = "${azurerm_resource_group.rg.name}"
-  location                 = "${var.location}"
+  count = var.azurerm_storage_account_name != "" ? 1 : 0
+  name  = var.azurerm_storage_account_name
+
+  #name_prefix              = "${var.name_prefix}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  tags                     = "${var.tags}"
+  tags                     = var.tags
 }
 
 module "loadbalancers" {
@@ -100,21 +103,21 @@ module "loadbalancers" {
   version = "~> 0.2.0"
 
   providers = {
-    azurerm = "azurerm"
+    azurerm = azurerm
   }
 
-  location                       = "${var.location}"
-  cluster_name                   = "${var.cluster_name}"
-  name_prefix                    = "${var.name_prefix}"
-  subnet_id                      = "${module.network.subnet_id}"
-  public_agents_additional_rules = ["${data.null_data_source.lb_rules.*.outputs}"]
-  masters_instance_nic_ids       = ["${module.masters.instance_nic_ids}"]
-  public_agents_instance_nic_ids = ["${module.public_agents.instance_nic_ids}"]
-  num_masters                    = "${var.num_masters}"
-  num_public_agents              = "${var.num_public_agents}"
+  location                       = var.location
+  cluster_name                   = var.cluster_name
+  name_prefix                    = var.name_prefix
+  subnet_id                      = module.network.subnet_id
+  public_agents_additional_rules = data.null_data_source.lb_rules.*.outputs
+  masters_instance_nic_ids       = module.masters.instance_nic_ids
+  public_agents_instance_nic_ids = module.public_agents.instance_nic_ids
+  num_masters                    = var.num_masters
+  num_public_agents              = var.num_public_agents
 
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-  tags                = "${var.tags}"
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = var.tags
 }
 
 module "bootstrap" {
@@ -122,27 +125,27 @@ module "bootstrap" {
   version = "~> 0.2.0"
 
   providers = {
-    azurerm = "azurerm"
+    azurerm = azurerm
   }
 
-  num_bootstrap                     = "${var.num_bootstrap}"
-  location                          = "${var.location}"
-  disk_size                         = "${coalesce(var.bootstrap_disk_size, var.infra_disk_size)}"
-  disk_type                         = "${coalesce(var.bootstrap_disk_type, var.infra_disk_type)}"
-  vm_size                           = "${coalesce(var.bootstrap_vm_size, var.infra_vm_size)}"
-  cluster_name                      = "${var.cluster_name}"
-  name_prefix                       = "${var.name_prefix}"
-  public_ssh_key                    = "${var.ssh_public_key_file}"
-  admin_username                    = "${coalesce(var.bootstrap_ssh_user, var.infra_ssh_user)}"
-  image                             = "${var.bootstrap_image}"
-  dcos_instance_os                  = "${coalesce(var.bootstrap_dcos_instance_os, var.infra_dcos_instance_os)}"
-  resource_group_name               = "${azurerm_resource_group.rg.name}"
-  subnet_id                         = "${module.network.subnet_id}"
-  network_security_group_id         = "${module.network-security-group.bootstrap.nsg_id}"
-  avset_platform_fault_domain_count = "${var.avset_platform_fault_domain_count}"
+  num_bootstrap                     = var.num_bootstrap
+  location                          = var.location
+  disk_size                         = coalesce(var.bootstrap_disk_size, var.infra_disk_size)
+  disk_type                         = coalesce(var.bootstrap_disk_type, var.infra_disk_type)
+  vm_size                           = coalesce(var.bootstrap_vm_size, var.infra_vm_size)
+  cluster_name                      = var.cluster_name
+  name_prefix                       = var.name_prefix
+  public_ssh_key                    = var.ssh_public_key_file
+  admin_username                    = coalesce(var.bootstrap_ssh_user, var.infra_ssh_user)
+  image                             = var.bootstrap_image
+  dcos_instance_os                  = coalesce(var.bootstrap_dcos_instance_os, var.infra_dcos_instance_os)
+  resource_group_name               = azurerm_resource_group.rg.name
+  subnet_id                         = module.network.subnet_id
+  network_security_group_id         = module.network-security-group.bootstrap_nsg_id
+  avset_platform_fault_domain_count = var.avset_platform_fault_domain_count
 
   # Determine if we need to force a particular location
-  tags = "${var.tags}"
+  tags = var.tags
 }
 
 module "masters" {
@@ -150,27 +153,27 @@ module "masters" {
   version = "~> 0.2.0"
 
   providers = {
-    azurerm = "azurerm"
+    azurerm = azurerm
   }
 
-  num_masters                       = "${var.num_masters}"
-  location                          = "${var.location}"
-  disk_size                         = "${coalesce(var.masters_disk_size, var.infra_disk_size)}"
-  disk_type                         = "${coalesce(var.masters_disk_type, var.infra_disk_type)}"
-  vm_size                           = "${coalesce(var.masters_vm_size, var.infra_vm_size)}"
-  cluster_name                      = "${var.cluster_name}"
-  name_prefix                       = "${var.name_prefix}"
-  public_ssh_key                    = "${var.ssh_public_key_file}"
-  admin_username                    = "${coalesce(var.master_ssh_user, var.infra_ssh_user)}"
-  image                             = "${var.masters_image}"
-  dcos_instance_os                  = "${coalesce(var.masters_dcos_instance_os, var.infra_dcos_instance_os)}"
-  resource_group_name               = "${azurerm_resource_group.rg.name}"
-  subnet_id                         = "${module.network.subnet_id}"
-  network_security_group_id         = "${module.network-security-group.masters.nsg_id}"
-  avset_platform_fault_domain_count = "${var.avset_platform_fault_domain_count}"
+  num_masters                       = var.num_masters
+  location                          = var.location
+  disk_size                         = coalesce(var.masters_disk_size, var.infra_disk_size)
+  disk_type                         = coalesce(var.masters_disk_type, var.infra_disk_type)
+  vm_size                           = coalesce(var.masters_vm_size, var.infra_vm_size)
+  cluster_name                      = var.cluster_name
+  name_prefix                       = var.name_prefix
+  public_ssh_key                    = var.ssh_public_key_file
+  admin_username                    = coalesce(var.master_ssh_user, var.infra_ssh_user)
+  image                             = var.masters_image
+  dcos_instance_os                  = coalesce(var.masters_dcos_instance_os, var.infra_dcos_instance_os)
+  resource_group_name               = azurerm_resource_group.rg.name
+  subnet_id                         = module.network.subnet_id
+  network_security_group_id         = module.network-security-group.masters_nsg_id
+  avset_platform_fault_domain_count = var.avset_platform_fault_domain_count
 
   # Determine if we need to force a particular location
-  tags = "${var.tags}"
+  tags = var.tags
 }
 
 module "private_agents" {
@@ -178,27 +181,30 @@ module "private_agents" {
   version = "~> 0.2.0"
 
   providers = {
-    azurerm = "azurerm"
+    azurerm = azurerm
   }
 
-  num_private_agents                = "${var.num_private_agents}"
-  location                          = "${var.location}"
-  disk_size                         = "${coalesce(var.private_agents_disk_size, var.infra_disk_size)}"
-  disk_type                         = "${coalesce(var.private_agents_disk_type, var.infra_disk_type)}"
-  vm_size                           = "${coalesce(var.private_agents_vm_size, var.infra_vm_size)}"
-  cluster_name                      = "${var.cluster_name}"
-  name_prefix                       = "${var.name_prefix}"
-  public_ssh_key                    = "${var.ssh_public_key_file}"
-  admin_username                    = "${coalesce(var.private_agent_ssh_user, var.infra_ssh_user)}"
-  image                             = "${var.private_agents_image}"
-  dcos_instance_os                  = "${coalesce(var.private_agents_dcos_instance_os, var.infra_dcos_instance_os)}"
-  resource_group_name               = "${azurerm_resource_group.rg.name}"
-  subnet_id                         = "${module.network.subnet_id}"
-  network_security_group_id         = "${module.network-security-group.private_agents.nsg_id}"
-  avset_platform_fault_domain_count = "${var.avset_platform_fault_domain_count}"
+  num_private_agents = var.num_private_agents
+  location           = var.location
+  disk_size          = coalesce(var.private_agents_disk_size, var.infra_disk_size)
+  disk_type          = coalesce(var.private_agents_disk_type, var.infra_disk_type)
+  vm_size            = coalesce(var.private_agents_vm_size, var.infra_vm_size)
+  cluster_name       = var.cluster_name
+  name_prefix        = var.name_prefix
+  public_ssh_key     = var.ssh_public_key_file
+  admin_username     = coalesce(var.private_agent_ssh_user, var.infra_ssh_user)
+  image              = var.private_agents_image
+  dcos_instance_os = coalesce(
+    var.private_agents_dcos_instance_os,
+    var.infra_dcos_instance_os,
+  )
+  resource_group_name               = azurerm_resource_group.rg.name
+  subnet_id                         = module.network.subnet_id
+  network_security_group_id         = module.network-security-group.private_agents_nsg_id
+  avset_platform_fault_domain_count = var.avset_platform_fault_domain_count
 
   # Determine if we need to force a particular location
-  tags = "${var.tags}"
+  tags = var.tags
 }
 
 module "public_agents" {
@@ -206,25 +212,28 @@ module "public_agents" {
   version = "~> 0.2.0"
 
   providers = {
-    azurerm = "azurerm"
+    azurerm = azurerm
   }
 
-  num_public_agents                 = "${var.num_public_agents}"
-  location                          = "${var.location}"
-  disk_size                         = "${coalesce(var.public_agents_disk_size, var.infra_disk_size)}"
-  disk_type                         = "${coalesce(var.public_agents_disk_type, var.infra_disk_type)}"
-  vm_size                           = "${coalesce(var.public_agents_vm_size, var.infra_vm_size)}"
-  cluster_name                      = "${var.cluster_name}"
-  name_prefix                       = "${var.name_prefix}"
-  public_ssh_key                    = "${var.ssh_public_key_file}"
-  admin_username                    = "${coalesce(var.public_agent_ssh_user, var.infra_ssh_user)}"
-  image                             = "${var.public_agents_image}"
-  dcos_instance_os                  = "${coalesce(var.public_agents_dcos_instance_os, var.infra_dcos_instance_os)}"
-  resource_group_name               = "${azurerm_resource_group.rg.name}"
-  subnet_id                         = "${module.network.subnet_id}"
-  network_security_group_id         = "${module.network-security-group.public_agents.nsg_id}"
-  avset_platform_fault_domain_count = "${var.avset_platform_fault_domain_count}"
+  num_public_agents = var.num_public_agents
+  location          = var.location
+  disk_size         = coalesce(var.public_agents_disk_size, var.infra_disk_size)
+  disk_type         = coalesce(var.public_agents_disk_type, var.infra_disk_type)
+  vm_size           = coalesce(var.public_agents_vm_size, var.infra_vm_size)
+  cluster_name      = var.cluster_name
+  name_prefix       = var.name_prefix
+  public_ssh_key    = var.ssh_public_key_file
+  admin_username    = coalesce(var.public_agent_ssh_user, var.infra_ssh_user)
+  image             = var.public_agents_image
+  dcos_instance_os = coalesce(
+    var.public_agents_dcos_instance_os,
+    var.infra_dcos_instance_os,
+  )
+  resource_group_name               = azurerm_resource_group.rg.name
+  subnet_id                         = module.network.subnet_id
+  network_security_group_id         = module.network-security-group.public_agents_nsg_id
+  avset_platform_fault_domain_count = var.avset_platform_fault_domain_count
 
   # Determine if we need to force a particular location
-  tags = "${var.tags}"
+  tags = var.tags
 }
--- outputs.tf
+++ outputs.tf
@@ -1,119 +1,124 @@
-output "bootstrap.admin_username" {
+output "bootstrap_admin_username" {
   description = "Bootstrap instance OS default user"
-  value       = "${module.bootstrap.admin_username}"
+  value       = module.bootstrap.admin_username
 }
 
-output "masters.admin_username" {
+output "masters_admin_username" {
   description = "Master instances OS default user"
-  value       = "${module.masters.admin_username}"
+  value       = module.masters.admin_username
 }
 
-output "private_agents.admin_username" {
+output "private_agents_admin_username" {
   description = "Private Agent instances OS default user"
-  value       = "${module.private_agents.admin_username}"
+  value       = module.private_agents.admin_username
 }
 
-output "public_agents.admin_username" {
+output "public_agents_admin_username" {
   description = "Public Agent instances OS default user"
-  value       = "${module.public_agents.admin_username}"
+  value       = module.public_agents.admin_username
 }
 
-output "bootstrap.private_ip" {
+output "bootstrap_private_ip" {
   description = "Private IP of the bootstrap instance"
-  value       = "${module.bootstrap.private_ip}"
+  value       = module.bootstrap.private_ip
 }
 
-output "bootstrap.public_ip" {
+output "bootstrap_public_ip" {
   description = "Public IP of the bootstrap instance"
-  value       = "${module.bootstrap.public_ip}"
+  value       = module.bootstrap.public_ip
 }
 
-output "bootstrap.nsg_id" {
+output "bootstrap_nsg_id" {
   description = "Network security group id of the bootstrap"
-  value       = "${module.network-security-group.bootstrap.nsg_id}"
+  value       = module.network-security-group.bootstrap_nsg_id
 }
 
-output "masters.public_ips" {
+output "masters_public_ips" {
   description = "Master instances public IPs"
-  value       = "${module.masters.public_ips}"
+  value       = module.masters.public_ips
 }
 
-output "masters.private_ips" {
+output "masters_private_ips" {
   description = "Master instances private IPs"
-  value       = "${module.masters.private_ips}"
+  value       = module.masters.private_ips
 }
 
-output "masters.nsg_id" {
+output "masters_nsg_id" {
   description = "Network security group id of the masters"
-  value       = "${module.network-security-group.masters.nsg_id}"
+  value       = module.network-security-group.masters_nsg_id
 }
 
-output "private_agents.public_ips" {
+output "private_agents_public_ips" {
   description = "Private Agent public IPs"
-  value       = "${module.private_agents.public_ips}"
+  value       = module.private_agents.public_ips
 }
 
-output "private_agents.private_ips" {
+output "private_agents_private_ips" {
   description = "Private Agent instances private IPs"
-  value       = "${module.private_agents.private_ips}"
+  value       = module.private_agents.private_ips
 }
 
-output "private_agents.nsg_id" {
+output "private_agents_nsg_id" {
   description = "Network security group id of the private agents"
-  value       = "${module.network-security-group.private_agents.nsg_id}"
+  value       = module.network-security-group.private_agents_nsg_id
 }
 
-output "public_agents.public_ips" {
+output "public_agents_public_ips" {
   description = "Public Agent public IPs"
-  value       = "${module.public_agents.public_ips}"
+  value       = module.public_agents.public_ips
 }
 
-output "public_agents.private_ips" {
+output "public_agents_private_ips" {
   description = "Public Agent instances private IPs"
-  value       = "${module.public_agents.private_ips}"
+  value       = module.public_agents.private_ips
 }
 
-output "public_agents.nsg_id" {
+output "public_agents_nsg_id" {
   description = "Network security group id of the public agents"
-  value       = "${module.network-security-group.public_agents.nsg_id}"
+  value       = module.network-security-group.public_agents_nsg_id
 }
 
-output "lb.masters" {
+output "lb_masters" {
   description = "lb address"
-  value       = "${module.loadbalancers.masters.lb_address}"
+  value       = module.loadbalancers.masters_lb_address
 }
 
-output "lb.masters-internal" {
+output "lb_masters-internal" {
   description = "lb address"
-  value       = "${module.loadbalancers.masters-internal.lb_address}"
+  value       = module.loadbalancers.masters-internal_lb_address
 }
 
-output "lb.public-agents" {
+output "lb_public-agents" {
   description = "lb address"
-  value       = "${module.loadbalancers.public-agents.lb_address}"
+  value       = module.loadbalancers.public-agents_lb_address
 }
 
 output "azurerm_storage_key" {
   description = "Azure Storage Account Access Keys for External Exhibitor"
-  value       = "${join(",", flatten(azurerm_storage_account.external_exhibitor.*.primary_access_key))}"
+  value = join(
+    ",",
+    flatten(
+      azurerm_storage_account.external_exhibitor.*.primary_access_key,
+    ),
+  )
 }
 
 output "vnet_id" {
   description = "ID of the VNET"
-  value       = "${module.network.vnet_id}"
+  value       = module.network.vnet_id
 }
 
 output "vnet_name" {
   description = "Name of the VNET"
-  value       = "${module.network.vnet_name}"
+  value       = module.network.vnet_name
 }
 
 output "resource_group_name" {
   description = "Name of the azure resource group"
-  value       = "${azurerm_resource_group.rg.name}"
+  value       = azurerm_resource_group.rg.name
 }
 
 output "subnet_id" {
   description = "Subnet ID"
-  value       = "${module.network.subnet_id}"
+  value       = module.network.subnet_id
 }
